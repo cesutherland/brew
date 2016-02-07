3890e4b7aaa34432e3e8646ec6560ba6e92a6569
Carl Sutherland <carl@humblesoftware.com>
2016-02-07 15:37:10 -0800

    Webpack hello.

---
+++ Hello.jsx
@@ -0,0 +1,16 @@
+/** @jsx React.DOM */
+
+'use strict'
+
+var React = require('react')
+
+module.exports = React.createClass({
+  displayName: 'HelloReact',
+  render: function(){
+    return (
+      <div>
+        <h1>HI GRACIE</h1>
+      </div>
+    );
+  }
+});
